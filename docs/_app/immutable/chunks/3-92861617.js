15e4123fca883bbef1a3cf2457207dd6f5293293
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-08-20 21:05:42 +0200

    try fix audioContext

--- docs/_app/immutable/chunks/3-92861617.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/sounds/_page.svelte-067d31c7.js";import"./index-c7d84774.js";import"./paths-6cd3a76e.js";export{r as component};
--- src/routes/sounds/+page.svelte
+++ src/routes/sounds/+page.svelte
@@ -5,57 +5,85 @@
 
 	export let count = 0;
 
+	//
+
+	let mambo: Howl | null = null;
+	let papi: Howl | null = null;
+
 	function playMambo() {
-		mambo.play();
+		if (mambo) {
+			mambo.play();
+		}
 	}
 
 	function playPapi() {
-		papi.play();
-	}
-
-	function seekMambo() {
-		console.log(mambo.seek() / mambo.duration());
-	}
-
-	let progress = 0;
-
-	function step() {
-		const seek = mambo.seek() || 0;
-		progress = (seek / mambo.duration()) * 100 || 0;
-		if (mambo.playing()) {
-			requestAnimationFrame(step);
+		if (papi) {
+			papi.play();
 		}
 	}
 
-	const mambo = new Howl({
-		src: [`${assets}/tracks/mambo.mp3`],
-		onplay: function () {
-			requestAnimationFrame(step);
+	function stopPapi() {
+		if (papi) {
+			papi.stop();
 		}
-	});
-
-	const papi = new Howl({
-		src: [`${assets}/tracks/papi.mp3`]
-		// onplay: function () {
-		// 	requestAnimationFrame(step);
-		// }
-	});
-
-	let papiClickable = true;
-
-	function update() {
-		papiClickable = false;
 	}
+
+	//
+
+	function createAudio() {
+		mambo = new Howl({
+			src: [`${assets}/tracks/mambo.mp3`]
+			// onplay: function () {
+			// 	requestAnimationFrame(step);
+			// }
+		});
+
+		papi = new Howl({
+			src: [`${assets}/tracks/papi.mp3`]
+			// onplay: function () {
+			// 	requestAnimationFrame(step);
+			// }
+		});
+	}
+
+	//
+
+	let visible = false;
+
+	function init() {
+		createAudio();
+		visible = true;
+	}
+
+	// function seekMambo() {
+	// 	console.log(mambo.seek() / mambo.duration());
+	// }
+
+	// let progress = 0;
+
+	// function step() {
+	// 	const seek = mambo.seek() || 0;
+	// 	progress = (seek / mambo.duration()) * 100 || 0;
+	// 	if (mambo.playing()) {
+	// 		requestAnimationFrame(step);
+	// 	}
+	// }
 </script>
 
-<div class="p-6 flex flex-col flex-nowrap items-stretch grow space-y-6">
-	<Button grow lock on:click={playPapi}>Papi</Button>
-
-	<Button grow disabled>{count}</Button>
-	<div class="grow shrink-0 flex flex-row flex-nowrap space-x-6">
-		<Button grow disabled>Mamacita</Button>
-		<Button grow lock disabled>Stop</Button>
+{#if !visible}
+	<div class="p-6 flex flex-col flex-nowrap items-stretch grow space-y-6">
+		<Button grow on:click={init}>Start</Button>
 	</div>
-	<hr class="border-t-2 border-t-black" />
-	<Button grow lock on:click={playMambo}>Mambo Salentino</Button>
-</div>
+{:else}
+	<div class="p-6 flex flex-col flex-nowrap items-stretch grow space-y-6">
+		<Button grow lock on:click={playPapi}>Papi</Button>
+
+		<Button grow disabled>{count}</Button>
+		<div class="grow shrink-0 flex flex-row flex-nowrap space-x-6">
+			<Button grow disabled>Mamacita</Button>
+			<Button grow on:click={stopPapi}>Stop</Button>
+		</div>
+		<hr class="border-t-2 border-t-black" />
+		<Button grow lock on:click={playMambo}>Mambo Salentino</Button>
+	</div>
+{/if}
